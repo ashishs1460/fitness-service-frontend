ed89b9a82f4bb64f82bfec4e374605e3a38516ce
Ashish S <ashish@Ashishs-MacBook-Air.local>
2024-05-30 18:58:40 +0530

    modify url render

--- src/app/services/services/extras.service.ts
+++ src/app/services/services/extras.service.ts
@@ -10,7 +10,7 @@ import { Observable } from 'rxjs';
 export class ExtrasService {
  
  
-    url:string = 'https://fitness-social-service.vercel.app/api/v1'
+    url:string = 'https://fitness-social-network.onrender.com/api/v1'
 
   constructor(
     private http : HttpClient
